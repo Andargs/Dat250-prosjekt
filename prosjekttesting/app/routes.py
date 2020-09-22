6212684244405d6ca0ea845b701843da78a4e085
Anders W Pettersen <ander@LAPTOP-A1VVRE2U.localdomain>
2020-09-22 10:10:06 +0200

    E

--- prosjekttesting/app/routes.py
+++ prosjekttesting/app/routes.py
@@ -1,6 +1,18 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-        return "Hello, World!"
+    user = {'username': 'Penal Berit'}
+    transaksasjoner = [
+        {
+            'sender': {'username': 'John'},
+            'mottaker': {'username': 'Grøtta grav'}
+        },
+        {
+            'sender': {'username': 'Susan'},
+            'mottaker': {'username': 'Gromlegrau'}
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, transaksasjoner=transaksasjoner)
