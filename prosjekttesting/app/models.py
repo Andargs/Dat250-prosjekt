b08d60a25b1f753187372394707741f70261fa07
Anders W Pettersen <ander@LAPTOP-A1VVRE2U.localdomain>
2020-09-22 17:06:25 +0200

    E

--- prosjekttesting/app/models.py
+++ prosjekttesting/app/models.py
@@ -6,7 +6,16 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    # konto = db.Column(db.Integer)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)   
+
+# class transak(db.Model)
+#     sum = db.Column(db.Integer, Primary_key=True)
+#     sender = db.Column(self.username)
+#     mottaker = db.Column(self.username)
+
+#     def __repr__(self):
+#        return '{} sendte {}kr til {}'.format(self.sender, self.sum, self.mottaker)
 
