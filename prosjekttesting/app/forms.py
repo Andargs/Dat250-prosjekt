d81b36e338772b52549753f039de94039c8cb7cc
SivertBrekke <sivertbrekkef@gmail.com>
2020-09-24 08:24:24 +0200

    added to forms

--- prosjekttesting/app/forms.py
+++ prosjekttesting/app/forms.py
@@ -7,3 +7,19 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+class RegisterForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    e_post = Stringfield('E-mail', validators=[Datarequired()])
+    submit = SubmitField('Register')
+
+class ForgotForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    e_post = StringField('E-mail', validators=[Datarequired()])
+    submit = SubmitField('Help me reset my password!')
+
+class TransactionForm(FlaskForm):
+    receiver = StringField('Receiver', validators=[DataRequired()])
+    ammount = IntField('Ammount', validators=[DataRequired()])
+    # From account: drop-down-meny med 
