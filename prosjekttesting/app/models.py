084754019e05f573803958bdfafe4a2796c4a2af
SivertBrekke <sivertbrekkef@gmail.com>
2020-09-25 13:46:58 +0200

    commit

--- prosjekttesting/app/models.py
+++ prosjekttesting/app/models.py
@@ -1,20 +1,24 @@
-from app import db
 from datetime import datetime
-from flask_login import UserMixin
-from app import login
-
-@login.user_loader
-def load_user(id):
-    return User.query.get(int(id))
+from app import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    transactions = db.relationship('Transaction', backref='author', lazy='dynamic')
 
     def __repr__(self):
-        return '<User {}>'.format(self.username)     
+        return '<User {}>'.format(self.username)
+
+class Transaction(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Transaction {}>'.format(self.body)
 
 #class transak(db.Model):
  #   body = db.Column(db.Integer)
