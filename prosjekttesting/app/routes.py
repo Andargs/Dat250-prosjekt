870198f6d4ab09632c2ee34b2cd467ffa315287b
SivertBrekke <sivertbrekkef@gmail.com>
2020-09-25 13:18:49 +0200

    m

--- prosjekttesting/app/routes.py
+++ prosjekttesting/app/routes.py
@@ -1,7 +1,7 @@
 
 from app import app
 from flask import render_template, flash, redirect
-from app.forms import LoginForm, RegisterForm
+from app.forms import LoginForm, RegistrationForm
 from app.models import User
 from flask_login import current_user, login_user, login_required
 
@@ -30,11 +30,17 @@ def logout():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    form = RegisterForm()
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
     if form.validate_on_submit():
-        flash('Registered for user {}'.format(
-            form.username.data))
-    return render_template('register.html', title='Sign up', form=form)
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
 
 @app.route('/')
 @app.route('/index')
