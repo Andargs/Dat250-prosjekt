8153c7e3e0d368737f8027a55186979921be3bf4
root <root@LAPTOP-FF187SF5.localdomain>
2020-10-17 14:18:29 +0200

    shit

--- prosjekttesting/app/models.py
+++ prosjekttesting/app/models.py
@@ -18,21 +18,19 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    #salt = db.Column(db.Integer)
+    
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
     def set_password(self, password):
         salt = get_random_bytes(8)
-        print(salt)
         hash = scrypt(password, salt, 32, 16384, 8, 1)
         basehash = base64.b64encode(hash)
         basehashstring = basehash.decode('utf-8')
         basesalt = base64.b64encode(salt)
         basesaltstring = basesalt.decode('utf-8')
         hashString = "{0};{1};{2};{3};{4}".format(basesaltstring, 16384, 8, 1, basehashstring)
-        print(hashString)
         self.password_hash = hashString
 
     def check_password(self, password):
@@ -40,15 +38,12 @@ class User(UserMixin, db.Model):
         deler = hashString.split(";",5)
         saltbyte = bytes(deler[0], 'utf-8')
         salt = base64.b64decode(saltbyte)
-        print(salt)
         hash = scrypt(password, salt, 32, 16384, 8, 1)
         basehash = base64.b64encode(hash)
         basehashstring = basehash.decode('utf-8')
         basesalt = base64.b64encode(salt)
         basesaltstring = basesalt.decode('utf-8')
         ny_hashString = "{0};{1};{2};{3};{4}".format(basesaltstring, 16384, 8, 1, basehashstring)
-        print(ny_hashString)
-        print(hashString)
         if ny_hashString == hashString:
             return True
         else:
